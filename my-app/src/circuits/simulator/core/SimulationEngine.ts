0573205566250703824fe10d463c5171646f6da5
Kyaw Zin Win <kyawzinwin23k@gmail.com>
2026-09-23 23:39:57 +0630

    fix(simulator): reset LDR digital sensor state with engine

--- my-app/src/circuits/simulator/core/SimulationEngine.ts
+++ my-app/src/circuits/simulator/core/SimulationEngine.ts
@@ -597,6 +597,7 @@ export class SimulationEngine {
     this.analogState = {
       pinVoltages: new Map(),
       pinValues: new Map(),
+      digitalOutputs: new Map(),
       conflicts: [],
     };
 
