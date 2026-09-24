06d9edec967c1cdd4fd5aeab0f438b7357f79bd7
Kyaw Zin Win <kyawzinwin23k@gmail.com>
2026-09-24 08:29:58 +0630

    fix(breadboard): normalize half rail handle ids

--- my-app/src/circuits/nodes/BreadboardHalfNode.tsx
+++ my-app/src/circuits/nodes/BreadboardHalfNode.tsx
@@ -282,9 +282,9 @@ function createBreadboardPins(): BreadboardPin[] {
           power.x,
 
         y,
+handleId:
 
-        handleId:
-          pinId,
+  `bf-${pinId.toLowerCase()}`,
       });
     });
   }
